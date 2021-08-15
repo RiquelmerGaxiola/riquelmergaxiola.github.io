d9fc7cb46e50acc90b13e7e7aa958cc9c4dd1798
RiquelmerGaxiola <54343167+RiquelmerGaxiola@users.noreply.github.com>
2021-08-15 17:55:51 -0600

    Delete js directory

--- js/parallax.js
+++
@@ -1,5 +0,0 @@
-var instance = M.Parallax.getInstance(elem);
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.parallax');
-  var instances = M.Parallax.init(elems, options);
-});
